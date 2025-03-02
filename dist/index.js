72333f129cc3ba8082bf0bcb5ea4329a6bc801fc
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-03-02 10:36:52 +0600

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const disPlay = (message) => {
+    let x = 25;
+    console.log("hello", message, x);
+};
+disPlay("tamim");
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const disPlay = (message: string) => {
+  let x: number = 25;
+  console.log("hello", message, x);
+};
+
+disPlay("tamim");
