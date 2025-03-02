17a70552a28516d937893bb8b236af1e76edc70d
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-03-02 12:18:20 +0600

    typescript bangla tutorial 18 : dom manipulation

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,16 @@
 "use strict";
-const disPlay = (message) => {
-    let x = 25;
-    console.log("hello", message, x);
-};
-disPlay("tamim");
+const form = document.querySelector(".form-submit");
+const userName = document.querySelector("#name");
+const userEmail = document.querySelector("#email");
+const userCountry = document.querySelector("#country");
+const userFeedback = document.querySelector("#feedback");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = {
+        userName: userName.value,
+        userEmail: userEmail.value,
+        userCountry: userCountry.value,
+        userFeedback: userFeedback.value,
+    };
+    console.log(data);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-const disPlay = (message: string) => {
-  let x: number = 25;
-  console.log("hello", message, x);
-};
+const form = document.querySelector(".form-submit") as HTMLFormElement;
 
-disPlay("tamim");
+const userName = document.querySelector("#name") as HTMLInputElement;
+const userEmail = document.querySelector("#email") as HTMLInputElement;
+const userCountry = document.querySelector("#country") as HTMLSelectElement;
+const userFeedback = document.querySelector("#feedback") as HTMLTextAreaElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  const data = {
+    userName: userName.value,
+    userEmail: userEmail.value,
+    userCountry: userCountry.value,
+    userFeedback: userFeedback.value,
+  };
+  console.log(data);
+});
