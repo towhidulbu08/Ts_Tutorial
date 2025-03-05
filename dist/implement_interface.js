8d7a9da0848fb11e9c9321049e7ae21344e1805d
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-03-05 10:46:18 +0600

    typescript bangla tutorial 21 : class implementing interface

--- dist/implement_interface.js
+++ dist/implement_interface.js
@@ -3,7 +3,10 @@ class User {
     constructor(fullName, age) {
         this.fullName = fullName;
         this.age = age;
+        this.formatUser = () => {
+            return `name: ${this.fullName}, age: ${this.age}`;
+        };
     }
 }
 let user = new User("Tamim", 27);
-console.log(user);
+console.log(user.formatUser());
--- src/implement_interface.ts
+++ src/implement_interface.ts
@@ -1,7 +1,14 @@
-class User {
+interface IUserFormatter {
+  formatUser: () => string;
+}
+
+class User implements IUserFormatter {
   constructor(private fullName: string, private age: number) {}
+  formatUser = () => {
+    return `name: ${this.fullName}, age: ${this.age}`;
+  };
 }
 
 let user = new User("Tamim", 27);
 
-console.log(user);
+console.log(user.formatUser());
